45bc2d74b0dbd29cbcb6177f7c1df1fbe2485d3c
Geetesh <moreofgeet25@gmail.com>
2023-10-08 17:59:11 +0530

    Updates

---
+++ precache-manifest.897186002aa953393de9f6470f026c2c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e1a6872f76f22ab3d66",
+    "url": "/portfolio/static/css/main.1b1cc330.chunk.css"
+  },
+  {
+    "revision": "9e1a6872f76f22ab3d66",
+    "url": "/portfolio/static/js/main.1accca13.chunk.js"
+  },
+  {
+    "revision": "4f8dfb8755505eabbdd9",
+    "url": "/portfolio/static/js/runtime~main.dc772a44.js"
+  },
+  {
+    "revision": "aa23c037935e83ba7af9",
+    "url": "/portfolio/static/css/2.1d35819c.chunk.css"
+  },
+  {
+    "revision": "aa23c037935e83ba7af9",
+    "url": "/portfolio/static/js/2.2e6c37fd.chunk.js"
+  },
+  {
+    "revision": "6cb4e366ddc73ab361e9",
+    "url": "/portfolio/static/js/3.e1886a8b.chunk.js"
+  },
+  {
+    "revision": "47a1ccb8d514f58ec0907ffb2e1c98e9",
+    "url": "/portfolio/static/media/bgcoding.47a1ccb8.png"
+  },
+  {
+    "revision": "b759d692b78bf828f6ff44d65390442f",
+    "url": "/portfolio/static/media/cap_photo.b759d692.jpeg"
+  },
+  {
+    "revision": "7180b885391d6c1ac839c0bc76a0ab93",
+    "url": "/portfolio/static/media/p3.7180b885.png"
+  },
+  {
+    "revision": "257afd71d43360d5d9732b32111010bc",
+    "url": "/portfolio/static/media/yt.257afd71.png"
+  },
+  {
+    "revision": "794800d9e696b276b353a313491427a9",
+    "url": "/portfolio/static/media/shop.794800d9.png"
+  },
+  {
+    "revision": "03f13506038d65c6ca17da9eadac68cc",
+    "url": "/portfolio/static/media/w.03f13506.png"
+  },
+  {
+    "revision": "b3d17f42aa35ac80244f3f93a0bd17e6",
+    "url": "/portfolio/static/media/nc.b3d17f42.png"
+  },
+  {
+    "revision": "e5d74d9fd24013edf6aae6919121c425",
+    "url": "/portfolio/static/media/ni.e5d74d9f.png"
+  },
+  {
+    "revision": "9e9d96d041d1acd4b36d15a417f78539",
+    "url": "/portfolio/static/media/ls.9e9d96d0.png"
+  },
+  {
+    "revision": "0360ed92c46e21eca70a425471e8e849",
+    "url": "/portfolio/static/media/chatify.0360ed92.png"
+  },
+  {
+    "revision": "d3df08688d0a36ae74b831727dbb5720",
+    "url": "/portfolio/static/media/dm.d3df0868.png"
+  },
+  {
+    "revision": "f05e01ebab9eb3737a322ac5da89a47a",
+    "url": "/portfolio/index.html"
+  }
+];
